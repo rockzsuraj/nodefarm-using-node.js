fb4f2c43345cb8b2044c25a39438918bfe97ebfb
suraj.kumar <skumar@dxlg.com>
2024-04-25 04:37:10 +0530

    feat: user & tour api

---
+++ natours/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/public`));
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
---
+++ natours/controllers/tourController.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`),
+);
+
+exports.checkID = (req, res, next, val) => {
+  if (tours.length < val) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'Invalid ID',
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'failed',
+      message: 'Missing name & price',
+    });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    requestedAt: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const newId = tours.length + 1;
+  const newTour = { id: newId, ...req.body };
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/../dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      console.log('err: ', err);
+      res.status(201).json({
+        status: 'success',
+        data: { tour: newTour },
+      });
+    },
+  );
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  res.status(200).json({
+    message: 'success',
+    data: { tour },
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '<Updated tour here...>',
+    },
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: null,
+  });
+};
